501a3dd8b5fea04968e04c808606a0a20cc2bb44
rasdinho <ras.dinho7@gmail.com>
2020-12-31 21:39:34 -0500

    add-increment

--- src/first-page/Increment.js
+++ src/first-page/Increment.js
@@ -10,11 +10,18 @@ class Increment extends React.Component {
             count: 0
         }
     }
- 
+    IncrementClickBtn = (e) =>{
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+
     render() {
         return (
 <div id="num">
-    
+    <h1>The Current count is: {this.state.count}</h1>
+    <button onClick={this.IncrementClickBtn}>Increment +1</button>
 </div>
         );
     }
