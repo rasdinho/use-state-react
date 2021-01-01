af926abae2c34a926e719b50eecd4c76d7d489af
rasdinho <ras.dinho7@gmail.com>
2020-12-31 21:38:28 -0500

    create-new-component

---
+++ src/first-page/Increment.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css'
+
+
+
+class Increment extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+ 
+    render() {
+        return (
+<div id="num">
+    
+</div>
+        );
+    }
+}
+
+export default Increment;
+
