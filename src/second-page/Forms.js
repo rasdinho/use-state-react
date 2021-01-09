d3ac2f79e639c3dbde1fc1ea9ee509e3dadb166d
rasdinho <ras.dinho7@gmail.com>
2021-01-09 01:29:22 -0500

    state-input

--- src/second-page/Forms.js
+++ src/second-page/Forms.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './style.css'
 
 class Forms extends Component {
+    constructor(){
+        super();
+        this.state = {
+            nameInput: "",
+            emailImput: "",
+            commentInput: ""
+
+        }
+    }
+
+
     render() {
         return (
 <div id="form-div">
-    <h1 id="form-h1"> Type Your Imformation Here</h1>
+    <h1 id="form-h1"> Forms and Inputs</h1>
      <form>      
-        <input name="name" type="text" class="feedback-input" placeholder="Name" />   
-        <input name="email" type="text" class="feedback-input" placeholder="Email" />
-        <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
+        <input name="name" type="text" value={this.state.nameInput} class="feedback-input" placeholder="Name" />   
+        <input name="email" type="text" value={this.state.emailImput} class="feedback-input" placeholder="Email" />
+        <textarea name="text" value={this.state.commentInput} class="feedback-input" placeholder="Comment"></textarea>
         <input type="submit" value="SUBMIT"/>
     </form>
 </div>
