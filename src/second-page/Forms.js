a868180335274e422190e8da4c66f67497e6681c
rasdinho <ras.dinho7@gmail.com>
2021-01-09 02:56:11 -0500

    state-callbacks

--- src/second-page/Forms.js
+++ src/second-page/Forms.js
@@ -12,15 +12,25 @@ class Forms extends Component {
         }
     }
 
+handleNameInput = () => {
 
+}
+
+handleEmailInput = () => {
+
+}
+
+handleCommentInput = () => {
+
+}
     render() {
         return (
 <div id="form-div">
     <h1 id="form-h1"> Forms and Inputs</h1>
      <form>      
-        <input name="name" type="text" value={this.state.nameInput} class="feedback-input" placeholder="Name" />   
-        <input name="email" type="text" value={this.state.emailImput} class="feedback-input" placeholder="Email" />
-        <textarea name="text" value={this.state.commentInput} class="feedback-input" placeholder="Comment"></textarea>
+        <input name="name" type="text" value={this.state.nameInput} class="feedback-input" placeholder="Name" onChange={this.handleNameInput}/>   
+        <input name="email" type="text" value={this.state.emailImput} class="feedback-input" placeholder="Email" onChange={this.handleEmailInput}/>
+        <textarea name="text" value={this.state.commentInput} class="feedback-input" placeholder="Comment" onChange={this.handleCommentInput}></textarea>
         <input type="submit" value="SUBMIT"/>
     </form>
 </div>
