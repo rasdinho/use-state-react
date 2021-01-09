d2d667658199f496644e7de4fd71ef020020c2a3
rasdinho <ras.dinho7@gmail.com>
2021-01-09 00:37:45 -0500

    add-form-html

--- src/second-page/Forms.js
+++ src/second-page/Forms.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import './style.css'
 
 class Forms extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+<div id="form-div">
+    <h1 id="form-h1"> Type Your Imformation Here</h1>
+     <form>      
+        <input name="name" type="text" class="feedback-input" placeholder="Name" />   
+        <input name="email" type="text" class="feedback-input" placeholder="Email" />
+        <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
+        <input type="submit" value="SUBMIT"/>
+    </form>
+</div>
         );
     }
 }
